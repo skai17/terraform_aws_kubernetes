fd09caf4f455a5f5335073c7d05bf9f0aba4dcde
Kai <skai17@gmx.de>
2020-01-07 12:12:24 +0100

    - added l4 elb test

--- infrastructure/test/nginx.tf
+++ infrastructure/test/nginx.tf
@@ -22,7 +22,6 @@ resource "null_resource" "kubectl_config" {
 
 locals {
   nginx_ingress_mandatory_yaml_url = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml"
-  nginx_ingress_lb_aws_yaml_url = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/aws/service-nlb.yaml"
 }
 
 data "http" "nginx_ingress_mandatory" {
@@ -44,31 +43,92 @@ resource "null_resource" "nginx_ingress_mandatory" {
 }
 
 ##################################################
-##          Establish nginx-ingress load balancer for aws
+##          Establish nginx-ingress network load balancer for aws
 ###################################################
 
-data "http" "nginx_ingress_lb_aws" {
+locals {
+  nginx_ingress_nlb_aws_yaml_url = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/aws/service-nlb.yaml"
+}
+
+data "http" "nginx_ingress_nlb_aws" {
   url = local.nginx_ingress_mandatory_yaml_url
 }
 
 resource "null_resource" "nginx_ingress_nlb_aws" {
   triggers = {
-    manifest_sha1 = sha1(data.http.nginx_ingress_lb_aws.body)
+    manifest_sha1 = sha1(data.http.nginx_ingress_nlb_aws.body)
   }
 
   // This creates a NLB in AWS
   provisioner "local-exec" {
-    command = "kubectl apply -f ${local.nginx_ingress_lb_aws_yaml_url}"
+    command = "kubectl apply -f ${local.nginx_ingress_nlb_aws_yaml_url}"
   }
 
   // Required for terraform destroy, because the LB in AWS must be deleted to finish it
   provisioner "local-exec" {
     when = destroy
-    command = "kubectl delete -f ${local.nginx_ingress_lb_aws_yaml_url}"
+    command = "kubectl delete -f ${local.nginx_ingress_nlb_aws_yaml_url}"
   }
 
   depends_on = [
     null_resource.nginx_ingress_mandatory
   ]
 }
+
+##################################################
+##          Establish nginx-ingress L4 load balancer for aws
+###################################################
+
+//locals {
+//  nginx_ingress_elb_aws_yaml_url1 = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/aws/service-l4.yaml"
+//  nginx_ingress_elb_aws_yaml_url2 = "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/aws/patch-configmap-l4.yaml"
+//}
+
+//data "http" "nginx_ingress_elb1_aws" {
+//  url = local.nginx_ingress_elb_aws_yaml_url1
+//}
+//data "http" "nginx_ingress_elb2_aws" {
+//  url = local.nginx_ingress_elb_aws_yaml_url2
+//}
+//
+//resource "null_resource" "nginx_ingress_elb1_aws" {
+//  triggers = {
+//    manifest_sha1 = sha1(data.http.nginx_ingress_elb1_aws.body)
+//  }
+//
+//  // This creates a NLB in AWS
+//  provisioner "local-exec" {
+//    command = "kubectl apply -f ${local.nginx_ingress_elb_aws_yaml_url1}"
+//  }
+//
+//  // Required for terraform destroy, because the LB in AWS must be deleted to finish it
+//  provisioner "local-exec" {
+//    when = destroy
+//    command = "kubectl delete -f ${local.nginx_ingress_elb_aws_yaml_url1}"
+//  }
+//}
+//
+//
+//resource "null_resource" "nginx_ingress_elb2_aws" {
+//
+//  triggers = {
+//    manifest_sha1 = sha1(data.http.nginx_ingress_elb2_aws.body)
+//  }
+//
+//  // This creates a NLB in AWS
+//  provisioner "local-exec" {
+//    command = "kubectl apply -f ${local.nginx_ingress_elb_aws_yaml_url2}"
+//  }
+//
+//  // Required for terraform destroy, because the LB in AWS must be deleted to finish it
+//  provisioner "local-exec" {
+//    when = destroy
+//    command = "kubectl delete -f ${local.nginx_ingress_elb_aws_yaml_url2}"
+//  }
+//
+//
+//  depends_on = [
+//    null_resource.nginx_ingress_mandatory
+//  ]
+//}
 
